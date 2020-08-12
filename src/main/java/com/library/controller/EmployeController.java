56c828fb60c7ffd937e009a09a8d8904714ce4e2
thir <thirdiallo@gmail.com>
2020-08-12 16:14:24 +0000

    Crud for Employe model

---
+++ src/main/java/com/library/controller/EmployeController.java
@@ -0,0 +1,140 @@
+package com.library.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.library.entities.Employe;
+import com.library.exceptions.ResourceNotFoundException;
+import com.library.services.EmployeService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/alAmine")
+public class EmployeController {
+	
+	@Autowired
+	private EmployeService employeService;
+	
+	@GetMapping("/employes")
+	public List<Employe> getAllEmployes() {
+		return employeService.findAllEmploye();
+		
+	}
+	
+	@GetMapping("/employes/{id}")
+	public ResponseEntity<Employe> getEmployeById(@PathVariable(value = "id") Long empId)
+			throws ResourceNotFoundException{
+		Employe employe = employeService.findEmployeById(empId)
+				.orElseThrow(()-> new ResourceNotFoundException("Employe that id is" + empId + "not found"));
+		return ResponseEntity.ok().body(employe);
+		
+	}
+	
+	@GetMapping("/searchEmployeByCni")
+	public Employe getEmployeByCni(@RequestParam(value = "cni") String cni) {
+		return employeService.findByCni(cni);
+		
+	}
+	
+	@GetMapping("/searchListEmployeByCni")
+	public List<Employe> getEmployesByCni(@RequestParam(value = "cni") String cni) {
+		
+		return employeService.ListEmployeByCni("%"+cni+"%");
+		
+	}
+	
+	@GetMapping("/searchEmployeByNom")
+	public Employe getEmployeByNom(@RequestParam(value = "name") String nom) {
+		return employeService.findByNom(nom);
+		
+	}
+	
+	@GetMapping("/searchListEmployeByNom")
+	public List<Employe> getEmployesByNom(@RequestParam(value = "name") String nom) {
+		
+		return employeService.ListEmployeByNom("%"+nom+"%");
+	}
+	
+	@GetMapping("/searchEmployeByPrenom")
+	public Employe getEmployeByPrenom(@RequestParam(value = "pren") String prenom) {
+		return employeService.findByPrenom(prenom);
+		
+	}
+	
+	@GetMapping("/searchListEmployeByPrenom")
+	public List<Employe> getEmployesByPrenom(@RequestParam(value = "pren") String prenom) {
+		
+		return employeService.ListEmployeByPrenom("%"+prenom+"%");
+	}
+	
+	@GetMapping("/searchEmployeByTelephone")
+	public Employe getEmployeByTelephone(@RequestParam(value = "tel") String telephone) {
+		return employeService.findByTelephone(telephone);
+		
+	}
+	
+	@GetMapping("/searchListEmployeByTelephone")
+	public List<Employe> getEmployesByTelephone(@RequestParam(value = "tel") String telephone) {
+		
+		return employeService.ListEmployeByTelephone("%"+telephone+"%");
+	}
+	
+	@GetMapping("/searchEmployeByEmail")
+	public Employe getEmployeByEmail(@RequestParam(value = "mail") String email) {
+		return employeService.findByEmail(email);
+		
+	}
+	
+	@GetMapping("/searchListEmployeByEmail")
+	public List<Employe> getEmployesByEmail(@RequestParam(value = "mail") String email) {
+		
+		return employeService.ListEmployeByEmail("%"+email+"%");
+	}
+	
+	@GetMapping("/searchListEmployeByPageable")
+	public Page<Employe> getAllEmployesByPageable(@RequestParam(name = "page") int page,
+			@RequestParam(name = "size") int size) {
+		return employeService.findAllEmployeByPage(PageRequest.of(page, size));
+	}
+	
+	@GetMapping(value = "/searchListEmployeByPageableParMotCle")
+	public Page<Employe> getAllEmployesByKeyWord(@RequestParam(name="mc", defaultValue="") String mc, 
+			@RequestParam(name = "page") int page,
+			@RequestParam(name = "size") int size) {
+		return employeService.findEmployeByKeyWord("%"+mc+"%", PageRequest.of(page, size));
+	}
+	
+	
+	@PostMapping("/employes") 
+	public Employe createEmploye(@RequestBody Employe employe) {
+		return employeService.saveEmploye(employe);	
+	}
+	
+	@PutMapping("/employes/{empId}")
+	public ResponseEntity<Employe>  updateEmploye(@PathVariable(value = "empId") Long empId, @RequestBody Employe employe) {
+		employe.setId(empId);
+		return new ResponseEntity<>(employeService.saveEmploye(employe), HttpStatus.OK);
+		
+	}
+	@DeleteMapping("/employes/{id}")
+	public ResponseEntity<Object> deleteEmploye(@PathVariable(value = "id") Long id) {
+		return employeService.deleteEmploye(id);
+		
+	}
+
+}
---
+++ src/main/java/com/library/services/EmployeServiceImpl.java
@@ -0,0 +1,136 @@
+package com.library.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.library.entities.Employe;
+import com.library.exceptions.ResourceNotFoundException;
+import com.library.repository.EmployeRepository;
+
+@Service
+@Transactional
+public class EmployeServiceImpl implements EmployeService {
+	
+	@Autowired
+	private EmployeRepository employeRepository;
+
+	@Override
+	public List<Employe> findAllEmploye() {
+		return employeRepository.findAll();
+	}
+
+	@Override
+	public Employe saveEmploye(Employe employe) {
+		return employeRepository.save(employe);
+	}
+
+	@Override
+	public Optional<Employe> findEmployeById(Long empId) {
+		if (!employeRepository.existsById(empId)) {
+			throw new ResourceNotFoundException("Employee N° " + empId + "not found");
+		}
+		
+		return employeRepository.findById(empId);
+	}
+
+	@Override
+	public Employe updateEmploye(Long empId, Employe employe) {
+		if (!employeRepository.existsById(empId)) {
+			throw new ResourceNotFoundException("Employee N° " + empId + "not found");
+		}
+		Optional<Employe> emp = employeRepository.findById(empId);
+		if (!emp.isPresent()) {
+			throw new ResourceNotFoundException("Employee N° " + empId + "not found");
+		}
+		
+		Employe empResult = emp.get();
+		
+		empResult.setCni(employe.getCni());
+		empResult.setNom(employe.getNom());
+		empResult.setPrenom(employe.getPrenom());
+		empResult.setAdresse(employe.getAdresse());
+		empResult.setTelephone(employe.getTelephone());
+		empResult.setEmail(employe.getEmail());
+		
+		return employeRepository.save(empResult);
+	}
+
+	@Override
+	public ResponseEntity<Object> deleteEmploye(Long empId) {
+		if (!employeRepository.existsById(empId)) {
+			throw new ResourceNotFoundException("Employe N° " + empId + "not found");
+		}
+		employeRepository.deleteById(empId);
+		
+		return ResponseEntity.ok().build();
+	}
+	
+	@Override
+	public Employe findByCni(String cni) {
+		return employeRepository.findByCni(cni);
+	}
+
+	@Override
+	public Employe findByNom(String nom) {
+		return employeRepository.findByNom(nom);
+	}
+
+	@Override
+	public Employe findByPrenom(String prenom) {
+		return employeRepository.findByPrenom(prenom);
+	}
+
+	@Override
+	public Employe findByEmail(String email) {
+		return employeRepository.findByEmail(email);
+	}
+
+	@Override
+	public Employe findByTelephone(String telephone) {
+		return employeRepository.findByTelephone(telephone);
+	}
+	
+	@Override
+	public List<Employe> ListEmployeByCni(String cni) {
+		return employeRepository.ListEmployeByCni(cni);
+	}
+
+	@Override
+	public List<Employe> ListEmployeByNom(String nom) {
+		return employeRepository.ListEmployeByNom(nom);
+	}
+
+	@Override
+	public List<Employe> ListEmployeByPrenom(String prenom) {
+		return employeRepository.ListEmployeByPrenom(prenom);
+	}
+
+	@Override
+	public List<Employe> ListEmployeByEmail(String email) {
+		return employeRepository.ListEmployeByEmail(email);
+	}
+
+	@Override
+	public List<Employe> ListEmployeByTelephone(String telephone) {
+		return employeRepository.ListEmployeByTelephone(telephone);
+	}
+
+	@Override
+	public Page<Employe> findAllEmployeByPage(Pageable page) {
+		return employeRepository.findEmployeByPageable(page);
+	}
+
+	@Override
+	public Page<Employe> findEmployeByKeyWord(String mc, Pageable pageable) {
+		return employeRepository.findEmployeByKeyWord(mc, pageable);
+	}
+
+}
