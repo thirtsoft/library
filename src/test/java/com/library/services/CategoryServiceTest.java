1c9b2c0fd1c9ae87adea457e16c8b2b26f386312
thir <thirdiallo@gmail.com>
2020-08-17 14:48:30 +0000

    Add

--- src/test/java/com/library/services/CategoryServiceTest.java
+++ src/test/java/com/library/services/CategoryServiceTest.java
@@ -8,11 +8,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.library.entities.Category;
 import com.library.repository.CategoryRepository;
 
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CategoryServiceTest {
 	
